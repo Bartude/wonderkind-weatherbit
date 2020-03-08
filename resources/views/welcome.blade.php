424ddd421b8d2d94b42f450ef320e4c4d5178cb5
Adam Silva <adamsilva01@gmail.com>
2020-03-08 23:17:13 +0000

    Created main layout. Created route for form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,12 +34,6 @@
                 position: relative;
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
                 text-align: center;
             }
@@ -48,51 +42,92 @@
                 font-size: 84px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
             .m-b-md {
                 margin-bottom: 30px;
+            }
+            .inline-block {
+                display: inline-block;
+            }
+            .block {
+                display: block;
+            }
+            .text-left {
+                text-align: left;
+            }
+            input {
+                padding: 5px 10px;
+                border-radius: 10px;
+                font-size: 18px;
+            }
+            label {
+                font-weight: 800;
+            }
+            .mb-10 {
+                margin-bottom: 10px;
+            }
+            button {
+                font-size: 18px;
+                padding: 10px;
+                border: none;
+                border-radius: 10px;
+                cursor: pointer;
+            }
+            .form-error {
+                color: red;
+                font-weight: bold;
+            }
+            .half-half {
+                display: flex;
+            }
+            .half-half div {
+                flex: 1 0 50%;
+            }
+            .bold {
+                font-weight: bold;
             }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wonderkind Test Case
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="half-half">
+                    <div>
+                        <form id="weatherForm" action="{{ route('average-forecast') }}" method="GET">
+                            <div class="text-left mb-10">
+                                <label class="block text-left" for="city">City Name</label>
+                                <input type="text" id="city" name="city" value="{{ old('city') }}" placeholder="For example, Amsterdam"/>
+                                @error('city')
+                                    <div>
+                                        <span class="form-error">{{ $message }}</span>
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="text-left mb-10">
+                                <label class="block text-left" for="countryCode">Two letter Country Code</label>
+                                <input type="text" name="countryCode" value="{{ old('countryCode') }}" placeholder="NL">
+                                @error('countryCode')
+                                    <div>
+                                        <span class="form-error">{{ $message }}</span>
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="text-left">
+                                <button type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        @if(session()->has('results'))
+                            <h2>Results for {{ session('city') . ', ' . session('countryCode')}} </h2>
+                            <h3>Current Average Temperature</h3>
+                            <span class="bold">{{ session('results')['currentAverage'] }} ºC</span>
+                            <h3>Average Temperature Next 10 Days</h3>
+                            <span class="bold">{{ session('results')['nextTenDays'] }} ºC</span>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
